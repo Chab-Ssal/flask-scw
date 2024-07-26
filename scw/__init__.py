f2c6f57ebc8d6f95ab30c2c1f59245653039bec0
강서준 <saejun502@gmail.com>
2024-07-26 14:59:37 +0900

    404 및 500 오류등록

--- scw/__init__.py
+++ scw/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -11,9 +11,14 @@ naming_convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
+
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+def server_error(e):
+    return render_template('500.html'), 500
 
 def create_app():
     app = Flask(__name__)
@@ -42,5 +47,9 @@ def create_app():
     # 필터
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
+
+    # 오류페이지
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, server_error)
 
     return app
