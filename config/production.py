8b3298285298d87ea6e1846d94dfb9e1523aa78a
강서준 <saejun502@gmail.com>
2024-07-26 12:08:38 +0900

    config 수정2

--- config/production.py
+++ config/production.py
@@ -1,5 +1,5 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'scw.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xfdh$\xa1\x86e\xfd\xf3[=\xed\xad\x06\xb9cJ'
